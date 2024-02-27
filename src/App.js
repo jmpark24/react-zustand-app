7611bac5b4a0e2b18ca1d5c09593a1b9bcd6ddfb
jmpark24 <jmpark@gmail.com>
2024-02-27 19:37:11 +0900

    persist, devtools, async 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
+import { useEffect } from 'react';
 import './App.css';
 import Counter from './components/Counter';
 import TodoList from './components/TodoList';
+import { useUserStore } from './store/useUserStore';
 
 function App() {
-  
+  const {fetch, user} = useUserStore();
+  console.log(user);
+
+  useEffect(() => {
+    fetch(1);
+  },[fetch]);
+
   return (
     <div className="App">
       <header className='App-header'>
         <Counter />
         <TodoList />
+        <p>
+          {user.name}
+        </p>
+        <p>
+          {user.phone}
+        </p>
       </header>
     </div>
   );
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,14 +1,20 @@
 import React from 'react'
-import useConterStore from '../store/useCounterStore';
+import { useConterStore } from '../store/useCounterStore';
 
 const Counter = () => {
   const { count, increment, reset, setNumber } = useConterStore();
+
+  const clear = () => {
+    useConterStore.persist.clearStorage();  
+    // store이름 하나 잡고 persist.clearStorage() 하면 storage 다 날라감, value만 사라지는게 아니라 그냥 다 사라짐
+  }
   return (
     <div>
       <p>{count}</p>
       <button onClick={increment}>one up</button>
       <button onClick={reset}>reset</button>
       <button onClick={() => setNumber(3)}>set number</button>
+      <button onClick={clear}>clear</button>
     </div>
   )
 }
